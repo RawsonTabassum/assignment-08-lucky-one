48b791edb14e44565bf4d42aefeae5b2164ed462
RawsonTabassum <rawson17113@gmail.com>
2022-03-27 22:52:56 +0600

    set missing id

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -27,21 +27,21 @@ const Cart = (props) => {
             <div className='row'>
                 <div>
                 {
-                    cart.map(item=> <Display cart={item}></Display>)
+                    cart.map(item=> <Display cart={item} key={item.id}></Display>)
                 }
                 </div>
             </div>
 
             <button onClick={() => selectedItem(0, cart.length - 1)} className='btn btn-outline-primary mb-3' type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Choose One For me</button>
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Your Selected Color</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Your Selected Color</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <h4>{item}</h4>
                         </div>
                     </div>
